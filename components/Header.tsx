2e6720483531e9427098a5d537f22aab7c384e2f
raghav161 <raghavbansal242003@gmail.com>
2024-07-21 22:25:46 +0530

    Changed title

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,22 +4,16 @@ import Link from 'next/link'
 
 const Header = ({ children, className }: HeaderProps) => {
   return (
-    <div className={cn("header", className)}>
-      <Link href='/' className="md:flex-1">
-        <Image 
-          src="/assets/icons/logo.svg"
-          alt="Logo with name"
-          width={120}
-          height={32}
-          className="hidden md:block"
-        />
+    <div className={cn("header flex items-center p-4", className)}>
+      <Link href='/' className="flex items-center md:flex-1">
         <Image 
           src="/assets/icons/logo-icon.svg"
           alt="Logo"
           width={32}
           height={32}
-          className="mr-2 md:hidden"
+          className="mr-2 hidden md:block"
         />
+        <span className="text-lg font-bold">DigitalDocs</span>
       </Link>
       {children}
     </div>
